b1388ab00671be19088b10a1c1ac14a63105f11d
Jef Fisher <jeffisher@Jefs-MacBook-Air.local>
2025-01-15 17:27:58 -0800

    make it better

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// WebSocket connection
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    // User joins a room
+    socket.on("joinRoom", (room) => {
+        console.log(`User ${socket.id} joined room: ${room}`);
+        socket.join(room);
+        socket.to(room).emit("message", { user: "Server", message: `${socket.id} joined the room.` });
+    });
+
+    // User leaves a room
+    socket.on("leaveRoom", (room) => {
+        console.log(`User ${socket.id} left room: ${room}`);
+        socket.leave(room);
+    });
+
+    // Handle WebRTC signaling messages
+    socket.on("signal", (data) => {
+        io.to(data.room).emit("signal", { user: socket.id, signal: data.signal });
+    });
+
+    // User disconnects
+    socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
